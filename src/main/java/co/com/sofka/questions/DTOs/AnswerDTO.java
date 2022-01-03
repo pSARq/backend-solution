3fc14da5429d11fa00d47b16d6ed8dfeebb83430
Santiago Almendra <santiagoalmendrarojas19@gmail.com>
2022-01-03 14:14:36 -0500

    Se agregaron los DTO

---
+++ src/main/java/co/com/sofka/questions/DTOs/AnswerDTO.java
@@ -0,0 +1,78 @@
+package co.com.sofka.questions.DTOs;
+
+import javax.validation.constraints.NotBlank;
+import java.util.Objects;
+
+public class AnswerDTO {
+    @NotBlank(message = "El userId no puede ser nulo")
+    private String userId;
+    @NotBlank(message = "La questionId no puede ser nulo")
+    private String questionId;
+    @NotBlank(message = "El answer no puede ser nulo")
+    private String answer;
+    private Integer position;
+
+    public AnswerDTO(){
+
+    }
+
+    public AnswerDTO(@NotBlank String userId, @NotBlank String questionId, @NotBlank String answer) {
+        this.userId = userId;
+        this.questionId = questionId;
+        this.answer = answer;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(String questionId) {
+        this.questionId = questionId;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AnswerDTO answerDTO = (AnswerDTO) o;
+        return Objects.equals(userId, answerDTO.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId);
+    }
+
+    @Override
+    public String toString() {
+        return "AnswerDTO{" +
+                "userId='" + userId + '\'' +
+                ", questionId='" + questionId + '\'' +
+                ", answer='" + answer + '\'' +
+                '}';
+    }
+}
