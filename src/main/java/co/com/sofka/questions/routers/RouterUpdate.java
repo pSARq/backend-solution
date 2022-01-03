63bc217dd4a466fb6dd46590671c193eafc4c323
Santiago Almendra <santiagoalmendrarojas19@gmail.com>
2022-01-03 17:23:56 -0500

    Se agregaron los routers

---
+++ src/main/java/co/com/sofka/questions/routers/RouterUpdate.java
@@ -0,0 +1,29 @@
+package co.com.sofka.questions.routers;
+
+import co.com.sofka.questions.model.QuestionDTO;
+import co.com.sofka.questions.usecases.UseCaseUpdate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.PUT;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
+public class RouterUpdate {
+
+    @Bean
+    public RouterFunction<ServerResponse> update(UseCaseUpdate useCaseUpdate){
+        return route(PUT("/update/{id}").and(accept(MediaType.APPLICATION_JSON)),
+                request -> request.bodyToMono(QuestionDTO.class)
+                        .flatMap(questionDTO -> useCaseUpdate.apply(questionDTO)
+                                .flatMap(result -> ServerResponse.ok()
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .bodyValue(result))
+                        )
+        );
+    }
+}
